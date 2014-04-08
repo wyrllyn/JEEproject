fb45f193f2dbee486a8d7fdae6028c6084a7487d
X3N0-Life-Form <adrarsis@laposte.net>
2014-04-08 14:43:32 +0200

    hkdfg

--- db-structure.sql
+++ db-structure.sql
@@ -3,3 +3,10 @@ create table Group(
 	name varchar(50),
 	primary key (group_id)
 );
+
+create table User(
+	user_id int not null auto_increment,
+	name unique varchar(50),
+	password varchar(50),
+	primary key (user_id)
+);
