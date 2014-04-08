7bafdd4633351ab01fc995614398eb9afb9d4f86
X3N0-Life-Form <adrarsis@laposte.net>
2014-04-08 13:55:56 +0200

    database stuff

---
+++ db-structure.sql
@@ -0,0 +1,5 @@
+create table Group(
+	group_id int not null auto_increment,
+	name varchar(50),
+	primary key (group_id)
+);
---
+++ src/controller/DatabaseInterface.java
@@ -0,0 +1,81 @@
+package controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class DatabaseInterface {
+	private String url;
+	private String user;
+	private String password;
+	
+	private Connection connection;
+	
+	private DBType dbType = DBType.MY_SQL;
+	
+	protected enum DBType {
+		MY_SQL,
+		SQLITE
+	}
+	
+	public DatabaseInterface(String url, String user, String password) {
+		super();
+		this.url = url;
+		this.user = user;
+		this.password = password;
+	}
+
+	protected void connect() {
+		try {
+			Class.forName( getDriverName() );
+			connection = DriverManager.getConnection( url, user, password );
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	protected void disconnect() {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected String getDriverName() {
+		switch (dbType) {
+		case MY_SQL:
+			return "com.mysql.jdbc.Driver";
+		case SQLITE:
+			return "org.sqlite.JDBC";
+		}
+		return null;
+	}
+	
+	// TODO: stuff that's gonna be useful
+	
+	public List<String> getGroupList() {
+		List<String> list = new ArrayList<String>();
+		try {
+			Statement statement = connection.createStatement();
+			String query = "SELECT name FROM Group;";
+			ResultSet result = statement.executeQuery(query);
+			while (result.next()) {
+				String name = result.getString("name");
+				list.add(name);
+			}
+			result.close();
+			statement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+}
