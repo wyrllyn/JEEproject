51af13b45a71d39febea109aaf8c302bac2d1c0b
X3N0-Life-Form <adrarsis@laposte.net>
2014-04-15 14:08:57 +0200

    DBI: added getTeacherByName

--- src/database/DatabaseInterface.java
+++ src/database/DatabaseInterface.java
@@ -259,4 +259,26 @@ public class DatabaseInterface {
 		return teachers;
 	}
 
+	public Person getTeacherByName(String name) {
+		try {
+			Statement statement = connection.createStatement();
+			String query = "SELECT User.id, name FROM Teacher, User"
+					+ " WHERE User.name = "
+					+ name + ";";
+			ResultSet result = statement.executeQuery(query);
+			result.next();
+			Person teacher = new Person();
+			int id = result.getInt("id");
+			teacher.setId(id);
+			
+			teacher.setName(result.getString("name"));
+			teacher.setPassword("");
+			teacher.setType("prof");
+			
+			return teacher;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
